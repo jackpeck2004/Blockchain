e631b869b0cb4ecc510ee4754ae7f65008c0063d
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-02-26 14:59:03 +0100

    main.py: changed block data so that it contains block i in loop

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from Blockchain import Blockchain
 blockchain = Blockchain("0000")
 
 for i in range(10):
-    mined_block = Block(blockchain.get_block_number(), blockchain.current_block.hash, ["Hello"])
+    mined_block = Block(blockchain.get_block_number(), blockchain.current_block.hash, "Block " + str(i + 1))
     mined_block.mine(blockchain.rule)
     blockchain.add(mined_block)
-
 
 while blockchain.head is not None:
     blockchain.head.print()
