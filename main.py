3aab02e362c772cfb9b6bdcbea27a8e417867675
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-02-26 15:26:52 +0100

    main.py created main function

--- main.py
+++ main.py
@@ -5,11 +5,16 @@ from Blockchain import Blockchain
 
 blockchain = Blockchain("0000")
 
-for i in range(10):
-    mined_block = Block(blockchain.get_block_number(), blockchain.current_block.hash, "Block " + str(i + 1))
-    mined_block.mine(blockchain.rule)
-    blockchain.add(mined_block)
+def  main():
+    for i in range(10):
+        mined_block = Block(blockchain.get_block_number(), blockchain.current_block.hash, "Block " + str(i + 1))
+        mined_block.mine(blockchain.rule)
+        blockchain.add(mined_block)
 
-while blockchain.head is not None:
-    blockchain.head.print()
-    blockchain.head = blockchain.head.next
+    while blockchain.head is not None:
+        blockchain.head.print()
+        blockchain.head = blockchain.head.next
+
+if __name__ == "__main__":
+    main()
+
