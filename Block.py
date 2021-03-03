7a2efd806493b3b53bb902cd0775b89925706dba
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-03-03 11:52:03 +0100

    added docstrings

--- Block.py
+++ Block.py
@@ -13,6 +13,12 @@ class Block:
         self.prev_hash = prev_hash
 
     def mine(self, rule):
+        """
+        Mines the nonce for the current block
+
+        :param rule: The rule to search for when mining
+        :type rule: str
+        """
         while True:
             self.calculate_hash()
             if self.hash.startswith(rule):
@@ -21,17 +27,25 @@ class Block:
                 self.nonce += 1
 
     def calculate_hash(self):
+        """
+        Calculates the hash for the Block
+
+        :return: Calculated Hash for the block
+        """
         # create string to be hashed
         str_to_hash = str(self.number) + "".join(self.data) + self.prev_hash + str(self.nonce)
 
         # calculate hash
         self.hash = hashlib.sha256(str_to_hash.encode()).hexdigest()
+        return self.hash
 
     def print(self):
+        """
+        Prints the block in a pretty format displaying all necessary information.
+        """
         print("Number: ", self.number)
         print("Nonce: ", self.nonce)
         print("Hash: ", self.hash)
         print("Prev Hash: ", self.prev_hash)
         print("Data: ", self.data)
         print("-------------------")
-
--- Blockchain.py
+++ Blockchain.py
@@ -18,18 +18,37 @@ class Blockchain:
         # point block to head
         self.current_block = self.head
 
-    def get_block_number(self) -> int:
+    def get_block_number(self):
+        """
+        Calculates the current block number and returns it
+
+        :return: current block number
+        :rtype: int
+        """
         self.blockNum += 1
         return self.blockNum
 
     def __block_can_be_added__(self, block):
+        """
+        Checks if a block respects the blockchain rule
+
+        :param block: the block to be checked
+        :return: if the block respects the blockchain rule or not
+        :rtype: bool
+        """
         if block.hash.startswith(self.rule):
             return True
         return False
 
     def add(self, block):
+        """
+        Checks if the block should be added to the blockchain and adds it to the blockchain
+
+        :param block: the block to be added
+        """
         if self.__block_can_be_added__(block):
             self.current_block.next = block
             self.current_block = self.current_block.next
         else:
+            print("ERROR: Block", block.hash, "cannot be added.")
             return
